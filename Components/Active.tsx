d9cc2ff36116bbe0c3386bccc89df6f22487cc5f
Your Name <pattaranan nuandua>
2023-02-20 18:16:15 +0700

    20 feb

--- Components/Active.tsx
+++ Components/Active.tsx
@@ -1,78 +1,74 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet,} from 'react-native';
 import DropdownPicker from 'react-native-dropdown-picker';
-import { BarChart, XAxis, YAxis, Grid } from 'react-native-svg-charts';
+import * as d3 from 'd3';
+import {Canvas,Path,Skia,useComputedValue,useFont,Text,Fill} from '@shopify/react-native-skia';
 
-const walkCount = [
-  {
-    "Week 1": {
-      Mon: 329,
-      Tue: 870,
-      Wed: 400,
-      Thu: 530,
-      Fri: 983,
-      Sat: 324,
-      Sun: 532,
-    },
-  },
-  {
-    "Week 2": {
-      Mon: 329,
-      Tue: 820,
-      Wed: 400,
-      Thu: 550,
-      Fri: 433,
-      Sat: 724,
-      Sun: 532,
-    },
-  },
+interface DataPoint {
+  label: string;
+  value: number;
+}
+
+const data: DataPoint[]=[
+    {label: 'monday',value:1200},
+    {label: 'tuesday',value:250},
+    {label: 'wensday',value:500},
+    {label: 'thursday',value:430},
+    {label: 'friday',value:740},
+    {label: 'saturday',value:900},
+    {label: 'sunday',value:300},
 ];
 
+const GRAPH_MAGIN = 20;
+const GRAPH_BAR_WIDTH = 9;
+const CanvasHeight= 350;
+const Canvaswidth= 400;
+const graphHeight = CanvasHeight - 2 * GRAPH_MAGIN;
+const graphWidth = Canvaswidth - 2 ;
+
 const Active = () => {
-  const [selectedWeek, setSelectedWeek] = useState(0);
-  const [weekData, setWeekData] = useState(walkCount[0]["Week 1"]);
-
-  const updateWeek = (index: number) => {
-    setSelectedWeek(index);
-    setWeekData(walkCount[index][Object.keys(walkCount[index])[0]]);
-  };
-
-  const days = Object.keys(weekData);
-  const values = Object.values(weekData);
-
+  const font = useFont(require('./Roboto-Bold.ttf'), 10);
+  const fontSize = 22;
+  const xDomain = data.map((dataPoint: DataPoint) => dataPoint.label);
+  const xRange = [0, graphWidth];
+  const x = d3.scalePoint().domain(xDomain).range(xRange).padding(1);
+  const yDomain = [
+    0,
+    d3.max(data, (yDataPoint: DataPoint) => yDataPoint.value),
+  ];
+  const yRange = [0, graphHeight];
+  const y = d3.scaleLinear().domain(yDomain).range(yRange);
+  const path = useComputedValue(() => {
+    const newPath = Skia.Path.Make();
+    data.forEach((dataPoint: DataPoint) => {
+      const rect = Skia.XYWHRect(
+        x(dataPoint.label) - GRAPH_BAR_WIDTH / 2,
+        graphHeight,
+        GRAPH_BAR_WIDTH,
+        y(dataPoint.value) * -1,
+      );
+      const rrect = Skia.RRectXY(rect, 8, 8);
+      newPath.addRRect(rrect);
+    });
+    return newPath;
+  },[])
+  if (!font) {
+    return <View />;
+  }
   return (
     <View style={styles.container}>
-      <DropdownPicker
-        items={walkCount.map((week, index) => ({ label: Object.keys(week)[0], value: index }))}
-        defaultValue={selectedWeek}
-        containerStyle={{ height: 40 }}
-        style={{ backgroundColor: '#fafafa' }}
-        itemStyle={{ justifyContent: 'flex-start' }}
-        dropDownStyle={{ backgroundColor: '#fafafa' }}
-        onChangeItem={item => updateWeek(item.value)}
-      />
-      <View style={styles.chartContainer}>
-        <YAxis
-          data={values}
-          contentInset={{ top: 20, bottom: 20 }}
-          svg={{ fontSize: 10, fill: 'grey' }}
-        />
-        <BarChart
-          style={{ height: 200, flex: 1 }}
-          data={values}
-          svg={{ fill: 'rgb(134, 65, 244)' }}
-          contentInset={{ top: 20, bottom: 20 }}
-        >
-          <Grid />
-        </BarChart>
-        <XAxis
-          style={{ marginHorizontal: -10 }}
-          data={values}
-          formatLabel={(value, index) => days[index]}
-          contentInset={{ left: 10, right: 10 }}
-          svg={{ fontSize: 10, fill: 'grey' }}
-        />
-      </View>
+      <Canvas style={styles.canvas}>
+        <Path path={path} color="#00979C"/>
+        {data.map((dataPoint: DataPoint)=>(
+          <Text
+          key={dataPoint.label}
+          font={font}
+          x={x(dataPoint.label)-10}
+          y={CanvasHeight - 25}
+          text={dataPoint.label}
+          />
+        ))}
+      </Canvas>
     </View>
   );
 };
@@ -80,13 +76,20 @@ const Active = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    justifyContent:'center',
+    alignSelf:'center',
   },
-  chartContainer: {
-    flexDirection: 'row',
-    height: 200,
-    padding: 20,
+  Text: {
+    //fontWeight:"700",
+    color:'#000',
+    //textAlign:'center',
+    //marginTop:200,
+    //marginBottom:30,
   },
+  canvas:{
+    height:CanvasHeight,
+    width:Canvaswidth,
+  }
 });
 
 export default Active;
